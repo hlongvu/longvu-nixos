ac598d7e6f4afdfb4b6f6fdb27c9b7d37d0d2ab6
Long Vu <hlongvu@gmail.com>
2025-11-13 10:16:46 +0700

    home folder

--- configuration.nix
+++ configuration.nix
@@ -118,16 +118,17 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
-	google-chrome
-	vscode
-	slack
-	telegram-desktop
-	localsend
-	cloudflare-warp
-	sublime4
-	sublime-merge
+    #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    wget
+    curl
+  	google-chrome
+  	vscode
+  	slack
+  	telegram-desktop
+  	localsend
+  	cloudflare-warp
+  	sublime4
+  	sublime-merge
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
--- flake.nix
+++ flake.nix
@@ -29,7 +29,7 @@
             home-manager.useUserPackages = true;
 
             # TODO replace ryan with your own username
-            home-manager.users.longvu = import ./home.nix;
+            home-manager.users.longvu = import ./home/home.nix;
 
             # Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix
           }
