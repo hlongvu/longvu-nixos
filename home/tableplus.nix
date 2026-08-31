806e5bfd50c65a1bd24e18350698d5c9f8b1f911
Long Vu <hlongvu@gmail.com>
2026-08-31 12:43:37 +0700

    update flake

--- home/tableplus.nix
+++ home/tableplus.nix
@@ -2,7 +2,7 @@
  { config, pkgs, ... }:
 
 let
-  sha = "sha256-CCj0VSR+WsGrFZYtVRLkYUIizgoJmyZTNTG3afHITVw=";
+  sha = "sha256-RxHzFzVhGB6+Q7CCTOVxcBvnp4+6JIfpvjTmSqxPQ8c=";
   pname = "tableplus"; # Required by appimageTools.extract
   version = "64";
   linkAppImageURL = "https://tableplus.com/release/linux/x${version}/TablePlus-x${version}.AppImage";
